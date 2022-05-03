1923c098461648eb8f319ea89d48b5738e66154a
INSAEN <pratham1986@gmail.com>
2022-05-03 19:12:00 +0530

    navbar ui done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React from "react";
 import md from "../images/md.png";
 import { Button } from "./Button";
+// import menico from "../images/menu.svg";
+import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
 export const Navbar = () => {
   let Links = [
     { name: "home", link: "/" },
@@ -9,6 +12,7 @@ export const Navbar = () => {
     { name: "contact", link: "/contact" },
     { name: "shop", link: "/shop" },
   ];
+  let [show, setShow] = React.useState(false);
   return (
     <nav className="shadow-md w-full fixed top-0 left-0">
       <div className="md:flex bg-[#13161E] py-4 md:px-10 px-7 text-white items-center justify-between">
@@ -16,9 +20,21 @@ export const Navbar = () => {
           <img src={md} alt="logo" className="h-14" />
           <span>myDrugs</span>
         </div>
-        <ul className="md:flex md:items-center font-spacemono md:pb-0 pb:12 md:static absolute bg-[#13161E] md:z-auto z-[-1] left-0 w-full md:w-auto">
+        <div
+          onClick={() => setShow(!show)}
+          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+        >
+          {show ? <IoClose /> : <IoMenu />}
+        </div>
+        <ul
+          className={`md:flex md:items-center font-spacemono md:pb-0 pb:12 md:static absolute bg-[#13161E] md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-300 ease-in ${
+            show
+              ? "top-20 opacity-100"
+              : "top-[-490px] opacity-0 md:opacity-100"
+          }`}
+        >
           {Links.map((link) => (
-            <li className="md:ml-8 text-sm md:my-0 my-7">
+            <li key={link.name} className="md:ml-8 text-sm md:my-0 my-7">
               <a
                 href={link.link}
                 className="text-white hover:text-gray-400 duration-300"
@@ -27,7 +43,7 @@ export const Navbar = () => {
               </a>
             </li>
           ))}
-          <li>
+          <li className="text-sm md:my-0 my-7">
             <Button name="login" />
           </li>
         </ul>
