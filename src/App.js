57edcd8563aad23b92542b6d475348f25592b09f
INSAEN <pratham1986@gmail.com>
2022-05-15 10:42:20 +0530

    pre recoil

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,7 +1,6 @@
 import { collection, doc, getDocs } from "firebase/firestore";
 import React, { useEffect } from "react";
 import { db } from "../firebase";
-import loading from "../images/loading.gif";
 const Shop = () => {
   const [meds, setMeds] = React.useState([]);
   useEffect(() => {
@@ -18,11 +17,16 @@ const Shop = () => {
   }, []);
   console.log(meds);
   return (
-    <div className="bg-dBlue min-w-full min-h-screen mt-20 text-white">
+    <div className="bg-dBlue min-w-full min-h-screen mt-[5.5rem] text-white">
       {meds.length === 0 ? (
         <div className="flex min-h-screen min-w-full justify-center items-center bg-loading"></div>
       ) : (
-        <div>{meds[0].name}</div>
+        <div>
+          {/* {meds[0].name} */}
+          <div className="md:text-4xl font-Montserrat font-bold text-center md:text-left text-2xl px-10 pt-10">
+            Our Products:
+          </div>
+        </div>
       )}
     </div>
   );
