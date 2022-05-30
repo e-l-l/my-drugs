ec10dec21c263d0fd8654944dea88762026dc8ce
neocollege <chemistry101.pn@gmail.com>
2022-05-30 19:41:29 +0530

    made contact, FAQ pages

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,58 @@
 import React from 'react'
+import DButton from "../components/DButton.js";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+        <div>
+        <div className='bg-[#13161E] min-h-screen flex flex-wrap  justify-evenly md:items-center md:justify-around pt-20 text-white'>
+            <div>
+            <div class="container-fluid px-0">
+    <img src="https://source.unsplash.com/1200x400/?call" alt="" className="w-full" />
+</div>
+<div className="container content-center items-center justify-center min-w-full px-3">
+    <h1 className="py-3 text-center font-spacemono text-4xl">contact us</h1>
+    <div className="">
+    <form method="post" action="/contact">
+        <div className="mb-3">
+            <label for="name" className="form-label font-spacemono px-3">name</label><br />
+            <input type="text" className="form-control px-3 mx-3 w-full font-spacemono block p-3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="name" name="name" placeholder="John Doe" />
+            {/* <div id="emailHelp" className="form-text">we'll never share your email with anyone else.</div> */}
+          </div>
+        <div className="mb-3">
+          <label for="email" className="form-label px-3 font-spacemono">email address</label><br />
+          <input type="email" className="form-control font-spacemono mx-3 w-full block p-3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="email" name="email" aria-describedby="emailHelp" placeholder="abc@example.com" />
+          <div id="emailHelp" className="form-text px-3 font-spacemono opacity-30">we'll never share your email with anyone else.</div>
+        </div>
+        <div className="mb-3">
+            <label for="phone" className="form-label px-3 font-spacemono">phone number</label><br />
+            <input type="number" className="form-control px-3 font-spacemono mx-3 w-full block p-3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" id="phone" name="phone" placeholder="+91 XXXXXXXXXX" />
+        </div>
+        <div className="mb-3">
+          
+<label for="message" className="block mb-2 font-medium text-white font-spacemono px-3">your message</label>
+<textarea id="message" rows="4" className="block p-3 w-full text-sm font-spacemono text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mx-3" placeholder="Leave a comment..."></textarea>
+
+        </div>
+        <div className="mb-3 form-check px-3">
+          <input type="checkbox" className="form-check-input px-3" id="exampleCheck1" />
+          <label className="form-check-label px-3 font-spacemono" for="exampleCheck1">confirm</label>
+        </div>
+        <div className="mb-3">
+        <div className='flex items-center justify-center'>
+        <DButton title="SUBMIT" href="/contact" />
+        </div>
+        
+        </div>
+        
+      </form>
+    </div>
+    
+</div>
+            </div>
+            
+        </div>
+        </div>
+        
   )
 }
 
--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -2,7 +2,99 @@ import React from 'react'
 
 const FAQ = () => {
   return (
-    <div>FAQ</div>
+    <div>
+      <div className='bg-[#13161E] min-h-screen font-spacemono'>
+        <div className="flex justify-center items-center">
+        <img src="https://source.unsplash.com/1200x400/?liminal" alt="" className='w-full' />
+        </div>
+
+      <section class="text-white">
+        <div class="container px-5 py-5 mx-auto">
+          <div class="text-center mb-20">
+            <h1 class="sm:text-3xl text-2xl font-spacemono text-center title-font text-white mb-4">
+              frequently asked questions
+            </h1>
+            <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+              The most common questions about how our business works and what
+              can do for you.
+            </p>
+          </div>
+          <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+            <div class="w-full lg:w-1/2 px-4 py-2">
+              <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4 text-black">
+                  How Long is this site live?
+                </summary>
+
+                <span>
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+              <details class="mb-4">
+                <summary class="font-semibold bg-gray-200 rounded-md py-2 px-4 text-black">
+                  Can I install/upload anything I want on there?
+                </summary>
+
+                <span>
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+              <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4 text-black">
+                  How can I migrate to another site?
+                </summary>
+
+                <span>
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+            </div>
+            <div class="w-full lg:w-1/2 px-4 py-2">
+              <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4 text-black">
+                  Can I change the domain you give me?
+                </summary>
+
+                <span class="px-4 py-2">
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+              <details class="mb-4 text-black">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                  How many sites I can create at once?
+                </summary>
+
+                <span class="px-4 py-2">
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+              <details class="mb-4">
+                <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4 text-black">
+                  How can I communicate with you?
+                </summary>
+
+                <span class="px-4 py-2">
+                  Laboris qui labore cillum culpa in sunt quis sint veniam.
+                  Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
+                  minim velit nostrud pariatur culpa magna in aute.
+                </span>
+              </details>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    </div>
   )
 }
 
