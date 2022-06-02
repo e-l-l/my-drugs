14ed7dd022dc539de3a1db49e6d5e05db6901c79
INSAEN <pratham1986@gmail.com>
2022-06-02 20:21:13 +0530

    post presentation commit

--- src/components/CartElement.js
+++ src/components/CartElement.js
@@ -1,9 +1,25 @@
+import { reload } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 import React from "react";
 import { AiFillPlusCircle } from "react-icons/ai";
 import { AiFillMinusCircle } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import { db } from "../firebase";
 
 const CartElement = (props) => {
+  // let navigate = useNavigate();
   const [count, setCount] = React.useState(1);
+  const noCart = async () => {
+    await setDoc(doc(db, "meds", props.id), {
+      c: false,
+      img: props.img,
+      name: props.name,
+      price: props.price,
+      quantity: props.qty,
+      type: props.type,
+    });
+    console.log("removed");
+  };
   console.log(props.name);
   return (
     <div>
@@ -29,8 +45,14 @@ const CartElement = (props) => {
           size={25}
           className="mx-1"
           onClick={() => {
-            if(count > 0){
-            setCount(count - 1);}
+            if (count > 1) {
+              setCount(count - 1);
+            } else if (count === 1) {
+              setCount(count - 1);
+              noCart();
+              // navigate("/cart");
+              window.location.reload();
+            }
           }}
         />
         {count}
